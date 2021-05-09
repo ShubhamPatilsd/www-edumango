299387788f8f9e724408fccb7423a56c90c3bc3c
ShubhamPatilsd <55328098+ShubhamPatilsd@users.noreply.github.com>
2021-05-09 14:52:13 -0700

    fixed the replaceAll error

--- src/modules/aboutus.tsx
+++ src/modules/aboutus.tsx
@@ -13,7 +13,7 @@ export default function AboutUs() {
         <div className="text-center space-y-6">
             <h1>About Us</h1>
                 <div className="w-96 mx-auto">
-                    <Card name="Shubham" avatar="https://cdn.discordapp.com/avatars/705186859105386517/9a49b9a6b096cbf6b41769ca6a2d8893.webp?size=128" description="Very cool gamer" position="Intro to Coding|Co-Website dev"/>
+                    <Card name="Shubham" avatar="https://cdn.discordapp.com/avatars/705186859105386517/9a49b9a6b096cbf6b41769ca6a2d8893.webp?size=128" description="Very cool gamer" position="Intro to Coding~Co-Website dev"/>
                 </div>
             </div>
       </>
