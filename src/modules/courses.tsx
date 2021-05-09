c2acd36bd13c7608654359d75fa30d9642167a0c
nikhilsmehta <nikhilsmehta7@gmail.com>
2021-05-09 11:40:24 -0700

    navbar updates

--- src/modules/courses.tsx
+++ src/modules/courses.tsx
@@ -4,7 +4,9 @@ import Image from 'next/image'
 export default function Courses() {
   return (
       <>
-        <Navbar/>
+        <div className="md:px-12 px-4">
+            <Navbar/>
+          </div>
         <div className="text-center">
             <h1>Courses</h1>
             </div>
