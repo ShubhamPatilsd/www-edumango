3c370536a8a85065f70014e80a4d089db2bab81d
nikhilsmehta <nikhilsmehta7@gmail.com>
2021-05-09 14:44:35 -0700

    card update

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -12,7 +12,7 @@ const Card:React.FC<CardProps> = ({avatar, name, description, position})=>{
     return <div className="shadow-around px-6 py-6 space-y-3 rounded-xl">
           <img className={avatar?"w-24 h-24 rounded-full mx-auto":"hidden"} src={avatar} alt={`${name}'s photo`} width="384" height="512"/>
           <h3 className={name ? "" : "hidden"}>{name}</h3>
-          <h5 className={position ? "font-bold" : "hidden"}>{position.replaceAll("|", " · ")}</h5>
+          <h5 className={position ? "font-bold" : "hidden"}>{position.replace("|", " · ")}</h5>
           <p className={description ? "" : "hidden"}>{description}</p>
           
     </div>
