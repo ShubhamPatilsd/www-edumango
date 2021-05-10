b4e4f70837130befa91712f60ac87985dc831a07
ShubhamPatilsd <55328098+ShubhamPatilsd@users.noreply.github.com>
2021-05-10 12:25:13 -0700

    changed shadow stuff

--- src/components/CourseCard.tsx
+++ src/components/CourseCard.tsx
@@ -29,7 +29,7 @@ const CourseCard: React.FC<CourseCardProps> = ({
   startingDate,
 }) => {
   return (
-    <div className="text-left shadow-around px-6 py-6 space-y-3 rounded-xl shadow-md hover:shadow-2xl transition transition-delay-15 ease-in-out">
+    <div className="text-left px-6 py-6 space-y-3 rounded-xl shadow-md hover:shadow-2xl transition transition-delay-15 ease-in-out">
       <h3 className={courseName ? "" : "hidden"}>{courseName}</h3>
 
       <h5
