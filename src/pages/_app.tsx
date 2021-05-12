cddfb43d8e8705ef42861999c397d52eb6812433
ShubhamPatilsd <55328098+ShubhamPatilsd@users.noreply.github.com>
2021-05-12 16:48:48 -0700

    added a favicon and changed the title in nextjs head tag

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,16 @@
 import "../styles/globals.css";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return(
+      <>
+      <Head>
+        <title>Edumango—Empowering Students</title>
+        <link rel="shortcut icon" href="/mango.png" />
+      </Head>
+      <Component {...pageProps} />;
+      </>
+  )
 }
 
 export default MyApp;
