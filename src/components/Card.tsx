e48a1e11b17b12cddbf82c6b7d3297687e7bc29a
ShubhamPatilsd <55328098+ShubhamPatilsd@users.noreply.github.com>
2021-05-12 19:04:33 -0700

    added gray border around staff cards

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -8,7 +8,7 @@ interface CardProps {
 
 const Card: React.FC<CardProps> = ({ avatar, name, description, position }) => {
   return (
-    <div className="shadow-md hover:shadow-2xl transition transition-delay-15 ease-in-out px-6 py-6 space-y-3 rounded-xl">
+    <div className="shadow-md border border-gray-100 hover:shadow-2xl transition transition-delay-15 ease-in-out px-6 py-6 space-y-3 rounded-xl">
       <img
         className={avatar ? "w-32 h-32 border-4 border-accent-primary rounded-full mx-auto" : "hidden"}
         src={avatar}
