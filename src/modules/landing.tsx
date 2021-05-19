e6f10bd1b33ce056e363a8f147f074dfb31f5f4d
nikhilsmehta <nikhilsmehta7@gmail.com>
2021-05-18 17:47:21 -0700

    I AM DEAD I AM DEAD I AM DEAD I AM DEAD I AM DEAD I AM DEAD I AM DEAD

--- src/modules/landing.tsx
+++ src/modules/landing.tsx
@@ -7,9 +7,10 @@ export default function Landing() {
       <div className="md:px-12 px-4">
         <Navbar />
       </div>
-      <div className="md:px-12 px-4 block justify-center space-y-4">
-        <h3>EduMango</h3>
-        <p>
+      <div className="md:px-12 px-4 space-y-4">
+        <h1 className="text-center text-accent-primary text-6xl">EduMango</h1>
+        <div className="grid justify-items-center">
+        <p className="float-right text-base sm:text-xl">
           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
           eiusmod tempor incididunt ut labore et dolore magna aliqua. Platea
           dictumst quisque sagittis purus sit amet volutpat. Mattis molestie a
@@ -24,17 +25,45 @@ export default function Landing() {
           consequat interdum varius sit amet mattis vulputate enim. Nulla
           posuere sollicitudin aliquam ultrices sagittis orci a scelerisque.
         </p>
-        <div className="grid justify-items-center">
-          <Image
-            src="/learning-tree.svg"
+
+        
+        <Image
+            
+            src="/education_pic.svg"
             alt="Learning"
-            width={500}
-            height={500}
+            width={450}
+            height={450}
             layout="intrinsic"
             quality={100}
           />
         </div>
-      </div>
+        
+        
+        {
+            //make it responsivse 
+        }
+        <div className="grid grid-cols-1 sm:grid-cols-3 divide-x">
+            
+            <div className="grid pr-8 sm:leading-none justify-items-center">
+                <img src="/school.svg" className="transition transition-delay-15 ease-in-out transform hover:-translate-y-2 w-24 h-24"/>
+                <p>hahha very funny pp tag</p>
+            </div>
+            
+            <div className="grid px-8 sm:leading-none justify-items-center">
+                <img src="/school.svg" className="transition transition-delay-15 ease-in-out transform hover:-translate-y-2 w-24 h-24"/>
+                <p>hahha very funny pp tag</p>
+            </div>
+            <div className="grid pl-8 sm:leading-none justify-items-center">
+                <img src="/school.svg" className="transition transition-delay-15 ease-in-out transform hover:-translate-y-2 w-24 h-24"/>
+                <p>hahha very funny pp tag</p>
+            </div>
+        </div>
+          
+          </div>
+          
+        
+      
+      
     </>
   );
 }
