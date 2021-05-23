9c72f689d3475c0ede35a810a329d44d2d0f6f37
nikhilsmehta <nikhilsmehta7@gmail.com>
2021-05-23 09:52:30 -0700

    added most of the form links

--- src/modules/courses.tsx
+++ src/modules/courses.tsx
@@ -14,7 +14,7 @@ export default function Courses() {
         "A fun and exciting class where students will learn the basics of Spanish 1, to help the students excel in their first year of Spanish.",
       startingDate: new Date('December 17, 1995 03:24 PST'),
       grades: [6, 7],
-      link: "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
+      link: "https://forms.gle/rzB91Am3eFg6xWhH7",
       days: "Tuesdays and Fridays"
     }, 
     {
@@ -22,7 +22,7 @@ export default function Courses() {
       courseDescription: "Utilizing the concepts learned in Spanish 1, explore the preterite tense, reflexive verbs, demonstrative adjectives, and much more. Needs completion of Spanish 1 in school, or in another accredited source.",
       startingDate: new Date("10 October 2011 11:30 PST"),
       grades: [7, 8],
-      link: "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
+      link: "https://forms.gle/5ET2pP4RE635toJS8",
       days: "Tuesdays and Fridays"
     },
     {
@@ -30,7 +30,7 @@ export default function Courses() {
       courseDescription: "Learning the basics to the most essential lessons, knowing how to speak the beautiful language to the grammar of the Central-European originated dialect, in an interesting and exciting course.",
       startingDate: new Date("05 October 2011 11:30"),
       grades: [6, 7],
-      link: "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
+      link: "https://forms.gle/5ET2pP4RE635toJS8",
       days: "Tuesdays"
     }
   ];
@@ -42,7 +42,7 @@ export default function Courses() {
         "In this interactive, entertaining, and informational class, we will guide the students through many different Grade Five math topics, enabling them to flourish in the future.",
       startingDate: new Date("05 October 2011 1:30 PM"),
       grades: [5],
-      link: "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
+      link: "https://forms.gle/W85Lt3wtTaSGDN8r8",
       days: "Mondays and Thursdays"
     },
     {
@@ -51,7 +51,7 @@ export default function Courses() {
         "A fun filled class preparing students for basic concepts in Pre-Algebra, to help them succeed in Middle school and High School!",
       startingDate: new Date("5 October 2011 1:30 PM"),
       grades: [6],
-      link: "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
+      link: "https://forms.gle/w9FPpujdYN6j5T3g7",
       days: "Mondays and Thursdays"
     },
     {
@@ -60,7 +60,7 @@ export default function Courses() {
         "An exciting class to get students ready for 7th Grade Math. We will uncover the surface of Pre-Algebra, diving into the most basic and important concepts of Pre-Algebra like inequalities, functions, and more!",
       startingDate: new Date("05 October 2011 1:30 PM"),
       grades: [7],
-      link: "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
+      link: "https://forms.gle/mosqHQJmG7P1hhFv9",
       days: "Mondays and Thursdays"
     },
     {
@@ -69,7 +69,7 @@ export default function Courses() {
         "In this class, students will learn all about Algebra 1 and the various topics within it. Algebra 1 is the gateway to high school curriculum so taking this class is very beneficial for students' futures!",
       startingDate: new Date("05 October 2011 1:30 PM"),
       grades: [8],
-      link: "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
+      link: "https://forms.gle/prRzHPu9x24YRMF17",
       days: "Mondays and Thursdays"
     }, 
   ];
@@ -81,7 +81,7 @@ export default function Courses() {
         "CAD or Computer Aided design is the process of designing things in 3D on your computer. This is so you can visualize the models before you make them in real life. Almost all tech companies use CAD for designing products, which is why it is such a valuable skill to learn.",
       startingDate: new Date("05 October 2011 11:30"),
       grades: [5, 8],
-      link: "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
+      link: "https://forms.gle/NcaF7LP9mT22P5Dh7",
       days: "Tuesdays"
     }, 
     {
@@ -102,7 +102,7 @@ export default function Courses() {
         "PlaceHolder Description",
       startingDate: new Date("05 October 2011 11:30"),
       grades: [6, 7],
-      link: "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
+      link: "https://forms.gle/5CMCsx2nAve3YeQ3A",
       days: "Mondays"
     }, 
     {
@@ -111,7 +111,7 @@ export default function Courses() {
         "A brand new course full of great opportunities where students can learn how to perform better in social environments, speak with confidence and present bold speeches. They will also learn how to debate in 2 different styles against each other. The course is recommended for 6th graders and up but 5th graders are welcome to join as well.",
       startingDate: new Date("05 October 2011 11:30"),
       grades: [5, 8],
-      link: "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
+      link: "https://forms.gle/i66BLjNYBdyuJVd38",
       days: "Fridays"
     }, 
   ];
