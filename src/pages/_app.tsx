17000148be4d074cab5214c09ace0547f5b8ee01
Shubham Patil <shubhampatilsd@gmail.com>
2021-06-14 17:23:00 -0700

    added head tag with cool image and description

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,7 +5,12 @@ function MyApp({ Component, pageProps }) {
   return(
       <>
       <Head>
-        <title>Edumango—Empowering Students Through Education</title>
+        <title>Edumango</title>
+        <meta charSet="UTF-8"/>
+        <meta property="og:image" content="/mango.png" />
+        <meta name="description" content="Empowering Students Through Education. EduMango is a teaching platform for kids from grades 5-8. We are currently providing eight-week camps over the summer for a variety of subjects and extracurriculars as well."/>
+        <meta name="keywords" content="Free, Education, Learning, Edumango"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
         <link rel="shortcut icon" href="/mango.png" />
       </Head>
       <Component {...pageProps} />
