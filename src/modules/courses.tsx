b853d6c048f1f4636e0ecc1177b260d41c40ad69
DevanshDaxini <devanshdaxini@gmail.com>
2022-06-14 15:49:29 +0530

    updates for 2022 and added "Former Members" section

--- src/modules/courses.tsx
+++ src/modules/courses.tsx
@@ -6,24 +6,26 @@ import CourseCardGroup from "../components/CourseCardGroup";
 
 export default function Courses() {
   const language = [
+   /* Format for Date: July __ , 2022 00:00 */
+   /* Updated the google form links, and make sure to update the Date when courses start and days the courses */
     {
       courseName: "Spanish 1",
       courseDescription:
         "A fun and exciting class where students will learn the basics of Spanish 1, to help the students excel in their first year of Spanish.",
-      startingDate: new Date("June 15, 2021 12:00"),
+      startingDate: new Date("N/A"),
       grades: [6, 8],
-      link: "https://forms.gle/rzB91Am3eFg6xWhH7",
-      days: "Tuesdays and Fridays",
+      link: "https://forms.gle/SjRXQytU2S2ry9jP7",
+      days: "Days - TBD",
     },
     {
       courseName: "Spanish 2",
       courseDescription:
         "Utilizing the concepts learned in Spanish 1, explore the preterite tense, reflexive verbs, demonstrative adjectives, and much more. Needs completion of Spanish 1 in school, or in another accredited source.",
-      startingDate: new Date("June 15, 2021 14:00 "),
+      startingDate: new Date("N/A"),
       grades: [7, 9],
-      link: "https://forms.gle/5ET2pP4RE635toJS8",
-      days: "Tuesdays and Fridays",
-    },
+      link: "https://forms.gle/SBkSyNLr2XQym6Mp9",
+      days: "Days - TBD",
+    }, 
   ];
 
   const math = [
@@ -31,37 +33,37 @@ export default function Courses() {
       courseName: "Math Grade 5",
       courseDescription:
         "In this interactive, entertaining, and informational class, we will guide the students through many different Grade Five math topics, enabling them to flourish in the future.",
-      startingDate: new Date("14 June 2021 14:00 "),
+      startingDate: new Date("N/A"),
       grades: [5],
-      link: "https://forms.gle/W85Lt3wtTaSGDN8r8",
-      days: "Mondays and Thursdays",
+      link: "https://forms.gle/u1eUQKudWEAucp44A",
+      days: "Days - TBD",
     },
     {
       courseName: "Math Grade 6",
       courseDescription:
         "A fun filled class preparing students for basic concepts in Pre-Algebra, to help them succeed in Middle school and High School!",
-      startingDate: new Date("14 June 2021 15:15 "),
+      startingDate: new Date("N/A"),
       grades: [6],
-      link: "https://forms.gle/w9FPpujdYN6j5T3g7",
-      days: "Mondays and Thursdays",
+      link: "https://forms.gle/sLWEn9KfNfebmKMh6",
+      days: "Days - TBD",
     },
     {
       courseName: "Pre-Algebra",
       courseDescription:
         "An exciting class to get students ready for 7th Grade Math. We will uncover the surface of Pre-Algebra, diving into the most basic and important concepts of Pre-Algebra like inequalities, functions, and more!",
-      startingDate: new Date("14 June 2021 18:00 "),
-      grades: [7],
-      link: "https://forms.gle/mosqHQJmG7P1hhFv9",
-      days: "Mondays and Fridays",
+      startingDate: new Date("N/A"),
+      grades: [7, 9],
+      link: "https://forms.gle/uKAcpBZnNF7jzghY7",
+      days: "Days - TBD",
     },
     {
       courseName: "Algebra 1",
       courseDescription:
         "In this class, students will learn all about Algebra 1 and the various topics within it. Algebra 1 is the gateway to high school curriculum so taking this class is very beneficial for students' futures!",
-      startingDate: new Date("15 June 2021 16:30 "),
-      grades: [8],
-      link: "https://forms.gle/prRzHPu9x24YRMF17",
-      days: "Tuesdays and Thursdays",
+      startingDate: new Date("N/A"),
+      grades: [8, 9],
+      link: "https://forms.gle/p2tUs6KWq9gLEqri6",
+      days: "Days - TBD",
     },
   ];
 
@@ -70,19 +72,28 @@ export default function Courses() {
       courseName: "Intro to CAD",
       courseDescription:
         "CAD or Computer Aided design is the process of designing things in 3D on your computer. This is so you can visualize the models before you make them in real life. Almost all tech companies use CAD for designing products, which is why it is such a valuable skill to learn.",
-      startingDate: new Date("15 June 2021 10:00 "),
+      startingDate: new Date("N/A"),
       grades: [6, 9],
-      link: "https://forms.gle/NcaF7LP9mT22P5Dh7",
-      days: "Tuesdays",
+      link: "https://forms.gle/mg7kKMjsN1vsSV3X8",
+      days: "Days - TBD",
     },
     {
       courseName: "Intro to Coding",
       courseDescription:
         "This class will get students started with the wonderful realm of coding. Students will learn a lot of concepts in the easiest and simplest way possible and know most of the AP Computer Science curriculum!",
-      startingDate: new Date("16 June 2021 16:00 "),
+      startingDate: new Date("N/A"),
       grades: [6, 9],
-      link: "https://forms.gle/sT4zMVfqRiMmZvwV7",
-      days: "Wednesdays",
+      link: "https://forms.gle/VdYvfg91mAUE6kct5",
+      days: "Days - TBD",
+    },
+    {
+      courseName: "Python",
+      courseDescription:
+        "Educating youth about Python, exploring algorithms, loops, and problem solving",
+      startingDate: new Date("N/A"),
+      grades: [5, 10],
+      link: "https://forms.gle/B9GfXnQczqNkbVBy6",
+      days: "Days - TBD",
     },
   ];
 
@@ -91,19 +102,19 @@ export default function Courses() {
       courseName: "Literature",
       courseDescription:
         "In this class students will read and study different genres, passages, and written works from different authors, teaching them to apply the concepts we learn into their writing.",
-      startingDate: new Date("14 June 2021 10:00 "),
-      grades: [6, 8],
-      link: "https://forms.gle/5CMCsx2nAve3YeQ3A",
-      days: "Mondays",
+      startingDate: new Date("N/A"),
+      grades: [6, 9],
+      link: "https://forms.gle/rhJGJrqyCoiUkcxM6",
+      days: "Days - TBD",
     },
     {
       courseName: "Speech & Debate",
       courseDescription:
         "A brand new course full of great opportunities where students can learn how to perform better in social environments, speak with confidence and present bold speeches. They will also learn how to debate in 2 different styles against each other.",
-      startingDate: new Date("18 June 2021 16:30 "),
-      grades: [5, 8],
-      link: "https://forms.gle/i66BLjNYBdyuJVd38",
-      days: "Fridays",
+      startingDate: new Date("N/A"),
+      grades: [5, 9],
+      link: "https://forms.gle/snCnF98FLfeK1iDe8",
+      days: "Days - TBD",
     },
   ];
 
@@ -112,16 +123,22 @@ export default function Courses() {
       <div className="md:px-12 px-4">
         <Navbar />
       </div>
-
+  
       <div className="space-y-6 md:px-12 px-4 md:mb-12 mb-4">
         <h1 className="text-center">Courses</h1>
         <h4 className="text-center">
           Click any of the courses below to register for that specific class.
         </h4>
+    {/* Update this year to year */}
         <p className="text-center">
           *All of the grades listed are referring to the student's grade for the
-          2021-2022 school year.
+          2022-2023 school year.
         </p>
+    {/* Update this year to year */}
+        <h5 className="text-center">
+          Some courses may not be taught for the summer of 2022
+        </h5>
+                    
 
         <div className="space-y-6">
           <CourseCardGroup
--- src/modules/staff.tsx
+++ src/modules/staff.tsx
@@ -3,103 +3,132 @@ import Card from "../components/Card";
 
 export default function AboutUs() {
   const leadStaffProfiles = [
+    
     {
       name: "Ishan Patel",
       avatar:
         "https://cdn.discordapp.com/attachments/832365353660317726/841128694139715624/unknown.png",
       description:
-        "Rising Freshman at American High School. Passionate about helping the community through education. Interested in Biology and US History.",
-      position: "Founder~President",
+        "Sophomore at American High School. Passionate about helping the community through education. Interested in Biology and US History.",
+      position: "Founder~Vice-President",
     },
+    
     {
       name: "Devansh Daxini",
       avatar:
         "https://cdn.discordapp.com/attachments/832365353660317726/841128925266837565/unknown.png",
       description:
-        "Rising Freshman at Washington High School who enjoys playing basketball. His favorite subject is math. Loves playing video games and enjoys video editing.",
-      position: "Co-Founder~Vice-President",
+        "Sophomore at Washington High School who enjoys playing basketball. His favorite subject is math. Loves playing video games and enjoys video editing.",
+      position: "Co-Founder~President",
     },
+    
     {
       name: "Bhagya Arora",
       avatar:
         "https://media.discordapp.net/attachments/832365353660317726/841454173882155008/unknown.png",
       description:
-        "Rising freshman at Foothill High School who loves building, coding, and biking. Enjoys learning new things, and sharing the knowledge with others.",
+        "Sophomore at Foothill High School who loves building, coding, and biking. Enjoys learning new things, and sharing the knowledge with others.",
       position: "Curriculum Lead~Marketing",
     },
+    
     {
       name: "Rushika Raval",
       avatar:
         "https://media.discordapp.net/attachments/832365353660317726/841454813786275880/unknown.png",
       description:
-        "8th grader at Horner Middle School who is passionate about biology, psychology, and English studies. Avid reader, who plays tabla (Indian Classical Drums), and does Bharatnatyam (Indian Dance).",
+        "9th grader at Horner Middle School who is passionate about biology, psychology, and English studies. Avid reader, who plays tabla (Indian Classical Drums), and does Bharatnatyam (Indian Dance).",
       position: "Co-Curriculum Lead",
     },
+    
     {
       name: "Tithi Raval",
       avatar:
         "https://media.discordapp.net/attachments/832365353660317726/841455384526323752/unknown.png",
       description:
-        "8th grader at Horner Middle School who is passionate about Java and Python programming as well as writing. Enjoys playing tabla (Indian classical drum), Bharatnatyam (Indian Dance), and art.",
+        "9th grader at Horner Middle School who is passionate about Java and Python programming as well as writing. Enjoys playing tabla (Indian classical drum), Bharatnatyam (Indian Dance), and art.",
       position: "Outreach Lead",
     },
+    
     {
       name: "Abhimanyu Sharma",
       avatar:
         "https://cdn.discordapp.com/attachments/832365353660317726/841712617977282580/unknown.png",
       description:
-        "Rising Freshman at Mission San Jose School. Enjoys Reading and Taekwondo. Has a deep interest in Coding and Engineering.",
+        "Sophomore at Mission San Jose School. Enjoys Reading and Taekwondo. Has a deep interest in Coding and Engineering.",
       position: "Expansion Lead",
     },
   ];
   const webDevProfiles = [
+    
     {
       name: "Shubham Patil",
       avatar:
         "https://cdn.discordapp.com/attachments/832365353660317726/841460312048795668/person.png",
       description:
-        "Rising Freshman at Mission San Jose High School who enjoys programming. Enjoys trying and eating food from multiple cuisines, playing videogames, and watching movies as well. He is very curious and likes to learn new things about technology.",
+        "Sophomore at Mission San Jose High School who enjoys programming. Enjoys trying and eating food from multiple cuisines, playing videogames, and watching movies as well. He is very curious and likes to learn new things about technology.",
       position: "Web Development",
     },
+   
     {
       name: "Nikhil Mehta",
       avatar:
         "https://media.discordapp.net/attachments/832365353660317726/841455861925937242/unknown.png",
       description:
-        "Rising Freshman at Mission San Jose High School who likes to play baseball, code, and enjoys doing CAD. Enjoys making apps and learning new skills.",
+        "Sophomore at Mission San Jose High School who likes to play baseball, code, and enjoys doing CAD. Enjoys making apps and learning new skills.",
       position: "Web Development",
     },
   ];
 
   const marketingProfiles = [
 
-
-    {
-      name: "Amay Singhania",
-      avatar:
-        "https://cdn.discordapp.com/attachments/775862443984551966/845806648505008158/unknown.png",
-      description:
-        "8th grader at Horner Middle School. Passionate about coding and Martial Arts. Enjoys reading and learning about technology.",
-      position: "Expansion",
-    },
-
     {
       name: "Rishabh Mahesh",
       avatar:
         "https://cdn.discordapp.com/attachments/832365353660317726/841129200584228904/unknown.png",
       description:
-        "Rising Freshman at Mission San Jose High School who is interested in helping others and giving back to the community whenever possible. Likes learning about technology biology. Actively participates in speech and debates and have been part of a school science bowl team.",
+        "Sophomore at Mission San Jose High School who is interested in helping others and giving back to the community whenever possible. Likes learning about technology biology. Actively participates in speech and debates and have been part of a school science bowl team.",
       position: "Expansion",
     },
   ];
 
   const instructorProfiles = [
+
+    {
+      name: "Utsav Agarwal",
+      avatar:
+        "https://media.discordapp.net/attachments/832365353660317726/841458341939380234/unknown.png",
+      description:
+        "Sophomore and Mission San Jose High School. Likes coding/robotics, math, and science. Participated in a school science bowl team and in multiple robotics competitions.",
+      position: "",
+    },
+
+    {
+      name: "Anish Polakala",
+      avatar:
+        "https://media.discordapp.net/attachments/762751394138554419/853349684428537856/unknown.png",
+      description:
+        "Sophomore at Washington High School. Loves coding, chess, math, and science. Exuberant reader.",
+      position: "",
+    },
+  ];
+
+  const formerProfiles = [
+    
+    {
+      name: "Amay Singhania",
+      avatar:
+        "https://cdn.discordapp.com/attachments/775862443984551966/845806648505008158/unknown.png",
+      description:
+        "9th grader who is passionate about coding and Martial Arts. Enjoys reading and learning about technology.",
+      position: "Expansion",
+    },
+    
     {
       name: "Akshat Gupta",
       avatar:
         "https://media.discordapp.net/attachments/832365353660317726/841456224533086238/unknown.png",
       description:
-        "Rising Freshman at Washington High School. Participated in a Tech Challenge team last year. Enjoys learning chess and world history.",
+        "Sophomore at Washington High School. Participated in a Tech Challenge team last year. Enjoys learning chess and world history.",
       position: "",
     },
 
@@ -108,7 +137,7 @@ export default function AboutUs() {
       avatar:
         "https://media.discordapp.net/attachments/832365353660317726/841456554058448946/unknown.png",
       description:
-        "Rising Freshman at Washington High School. Loves to swim, dance, sing, and make videos. Has more than 500 subscribers on YouTube.",
+        "Sophomore at Washington High School. Loves to swim, dance, sing, and make videos. Has more than 500 subscribers on YouTube.",
       position: "",
     },
 
@@ -117,7 +146,7 @@ export default function AboutUs() {
       avatar:
         "https://media.discordapp.net/attachments/832365353660317726/841458011967848468/unknown.png",
       description:
-        "Rising Freshman at Mission San Jose High School. Extremely passionate about guitar and music production! Favorite subject is English, and has a dream is to be an attorney.",
+        "Sophomore at Mission San Jose High School. Extremely passionate about guitar and music production! Favorite subject is English, and has a dream is to be an attorney.",
       position: "",
     },
 
@@ -126,7 +155,7 @@ export default function AboutUs() {
       avatar:
         "https://media.discordapp.net/attachments/832365353660317726/841456928714784778/unknown.png",
       description:
-        "Rising Freshman at Mission San Jose High School. Enjoys reading, science, and math. Likes Indian classical dance and singing as well as tennis and playing the viola.",
+        "Sophomore at Mission San Jose High School. Enjoys reading, science, and math. Likes Indian classical dance and singing as well as tennis and playing the viola.",
       position: "",
     },
 
@@ -135,35 +164,12 @@ export default function AboutUs() {
       avatar:
         "https://media.discordapp.net/attachments/832365353660317726/841457774982594570/unknown.png",
       description:
-        "Rising Freshman at Mission San Jose High School. Enjoys making music, doing any style of art, roller skating, and reading. Loves spending time with her family and helping out whenever she can.",
+        "Sophomore at Mission San Jose High School. Enjoys making music, doing any style of art, roller skating, and reading. Loves spending time with her family and helping out whenever she can.",
       position: "",
     },
 
-    {
-      name: "Utsav Agarwal",
-      avatar:
-        "https://media.discordapp.net/attachments/832365353660317726/841458341939380234/unknown.png",
-      description:
-        "Rising freshman and Mission San Jose High School. Likes coding/robotics, math, and science. Participated in a school science bowl team and in multiple robotics competitions.",
-      position: "",
-    },
-    {
-      name: "Rishabh Manur",
-      avatar:
-        "https://media.discordapp.net/attachments/832365353660317726/841458907310587914/unknown.png",
-      description:
-        "Rising freshman at Mission San Jose High School. In his spare time, likes playing the tabla, playing tennis, listening to music, and gardening. Enjoys coding, robotics, and hanging out with his friends.",
-      position: "",
-    },
-    {
-      name: "Anish Polakala",
-      avatar:
-        "https://media.discordapp.net/attachments/762751394138554419/853349684428537856/unknown.png",
-      description:
-        "Rising Freshman at Washington High School. Loves coding, chess, math, and science. Exuberant reader.",
-      position: "",
-    },
   ];
+
 
   return (
     <>
@@ -236,6 +242,25 @@ export default function AboutUs() {
             <h2 className="text-accent-primary">Instructors</h2>
             <div className="grid md:grid-cols-2  lg:grid-cols-3 sm:grid-cols-2 gap-4">
               {instructorProfiles.map(
+                ({ name, avatar, description, position }, i) => {
+                  return (
+                    <Card
+                      key={i}
+                      name={name}
+                      avatar={avatar}
+                      description={description}
+                      position={position}
+                    />
+                  );
+                }
+              )}
+            </div>
+          
+          </div>
+          <div className="space-y-4">
+            <h2 className="text-accent-primary">Former Members</h2>
+            <div className="grid md:grid-cols-2  lg:grid-cols-3 sm:grid-cols-2 gap-4">
+              {formerProfiles.map(
                 ({ name, avatar, description, position }, i) => {
                   return (
                     <Card
